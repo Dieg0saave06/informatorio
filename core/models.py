e214a49dd93c1e7f36ca74ac7bdb65ac400d209a
Diego <Diegoosaavedra2006@gmail.com>
2023-07-04 23:51:28 -0300

    vamos de nuevo2

--- core/models.py
+++ core/models.py
@@ -2,7 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Publicaciones(models.Model):
-    fecha = models.DateField()
-    titulo = models.CharField(max_length = 255)
-    post = models.TextField()
